0970b9bd362d69dd8d1a4b1700efe2f8b9fe25bc
randgalt <jordan@jordanzimmerman.com>
2016-08-15 17:05:01 -0500

    added doc/comments

--- example/src/main/java/io/soabase/web/ExampleMain.java
+++ example/src/main/java/io/soabase/web/ExampleMain.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * A standard Dropwizard application/main
+ */
 public class ExampleMain extends Application<ExampleConfiguration>
 {
     public static void main(String[] args) throws Exception
@@ -21,14 +24,15 @@ public class ExampleMain extends Application<ExampleConfiguration>
         }
 
         ExampleMain main = new ExampleMain();
-        System.setProperty("dw.web.assetsFile", args[0]);
-        System.setProperty("dw.web.debug", "true");
+        System.setProperty("dw.web.assetsFile", args[0]);   // normally config is set via a config
+        System.setProperty("dw.web.debug", "true");         // file - but for this example this was easier
         main.run("server");
     }
 
     @Override
     public void initialize(Bootstrap<ExampleConfiguration> bootstrap)
     {
+        // The config accessor allows Soabase Web to get its config
         ConfigAccessor<ExampleConfiguration> configAccessor = new ConfigAccessor<ExampleConfiguration>()
         {
             @Override
@@ -40,7 +44,7 @@ public class ExampleMain extends Application<ExampleConfiguration>
             @Override
             public Optional<WebConfiguration> getAdminConfiguration(ExampleConfiguration configuration)
             {
-                return Optional.empty();
+                return Optional.empty();    // in this example, we're not using Sobase Web for admin
             }
         };
         bootstrap.addBundle(new WebBundle<>(this::makeModel, configAccessor));
@@ -48,6 +52,11 @@ public class ExampleMain extends Application<ExampleConfiguration>
 
     private Object makeModel(HttpServletRequest request, String languageCode)
     {
+        /*
+            Use this technique to have language specific assets. See the "img"
+            directory. There is a "en" directory and a "es" directory. Both
+            have an image with the same name but the actual image is language specific.
+         */
         Map<String, String> model = Maps.newHashMap();
         model.put("lang", languageCode);
         return model;
